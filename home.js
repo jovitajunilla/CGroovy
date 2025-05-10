ab3c7dfdb8def9855f795aabb2793bdbe262190f
Qabil <leancyleancy5@gmail.com>
2025-05-10 21:26:46 +0700

    perubahan terbaru homepage

---
+++ home.js
@@ -0,0 +1,43 @@
+import {songData} from "./song-data.js";
+
+displaySong();
+
+function displaySong(){
+    const contentGroup = document.getElementById("content-group");
+    const contentGroupRecommendation = document.getElementById("content-group-recommendation");
+    
+    songData.forEach(song => {
+        const songBox = document.createElement("div");
+        songBox.classList.add("each-content");
+
+        songBox.innerHTML = `<img src="${song.image}" alt="" />
+                  <div class="content-teks">
+                    <h4>${song.title}</h4>
+                    <h5>${song.artist}</h5>
+                  </div>`;
+        
+        const songBoxClone = songBox.cloneNode(true);
+
+        contentGroupRecommendation.appendChild(songBox);
+        contentGroup.appendChild(songBoxClone);
+
+        songBox.addEventListener("click", function(){
+            localStorage.setItem("selectedSong", JSON.stringify(song));
+            window.location.href="song-detail.html";
+        });
+
+        songBoxClone.addEventListener("click", function(){
+            localStorage.setItem("selectedSong", JSON.stringify(song));
+            window.location.href="song-detail.html";
+        });
+
+    });    
+}
+
+// contentGroup.innerHTML = `<div class="each-content">
+//               <img src="img/gambar1-bestseller.png" alt="" />
+//               <div class="content-teks">
+//                 <h4>Talking To The Moon</h4>
+//                 <h5>Bruno Mars</h5>
+//               </div>
+//             </div>`
