6297b74d711c2e4171b823b781bc2a2fe4ebf41e
jovita <jovitahun123@gmail.com>
2025-05-16 12:49:38 +0700

    add hover

--- home.js
+++ home.js
@@ -23,8 +23,8 @@ function displaySong(){
         // contentGroupRecommendation.appendChild(songBoxClone);
         
         songBox.addEventListener("click", function(){
-            // localStorage.setItem("selectedSong", JSON.stringify(song));
-            // window.location.href="song-detail.html";
+            localStorage.setItem("selectedSong", JSON.stringify(song));
+            window.location.href="song-detail.html";
 
             
         });
