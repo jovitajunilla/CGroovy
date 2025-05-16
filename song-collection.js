5f4480a26a1a919b026b1c0ca3ceb4ca645a3098
jovita <jovitahun123@gmail.com>
2025-05-16 10:51:47 +0700

    add play bar

--- song-collection.js
+++ song-collection.js
@@ -41,12 +41,46 @@ function displaySong() {
             <p class="song-release">Year Date: ${song.release}</p>
             <p class="song-mood">Mood: ${song.mood}</p>`;
 
-            songDiv.addEventListener("click",function(){
-                console.log(song)
-                
-                localStorage.setItem("selectedSong",JSON.stringify(song));
-                window.location.href="song-detail.html";
-            });
+      songDiv.addEventListener("click", function () {
+        console.log(song);
+        const play = document.getElementById("play");
+        play.style.display = "flex";
+
+        play.innerHTML = ` 
+            <img src="${song.image}" alt="" id="image-mini">
+          <div id="play-2">
+            <p id="play-title">${song.title}</p>
+            <p>${song.artist}</p>
+          </div>
+          <a href="song-detail.html">
+          <img src="img/maximize.png" alt="" class="image-mini2">
+          </a>
+          <img src="img/play-icon-2.png" alt="" class="image-mini2" id="play-icon">
+          <audio src="${song.audio}" id="song-audio" preload="auto"></audio>`;
+
+        localStorage.setItem("selectedSong",JSON.stringify(song));
+        // window.location.href="song-detail.html";
+        
+        const playIcon = document.getElementById("play-icon");
+        const songAudio = document.getElementById("song-audio");
+        
+        songAudio.play();
+        playIcon.src = "img/pause-icon.png";
+        
+        playIcon.addEventListener("click",function(){
+          if(songAudio.paused){
+            songAudio.play();
+            playIcon.src = "img/pause-icon.png";
+          }else{
+            songAudio.pause();
+            playIcon.src = "img/play-icon-2.png";
+          }
+        })
+
+        songAudio.addEventListener("ended",function(){
+          playIcon.src = "img/play-icon-2.png";
+        })
+      });
       listSong.appendChild(songDiv);
     });
 
